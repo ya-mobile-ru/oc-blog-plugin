9b595d30f60e5310aeff3edb38a285e436f9ffe6
Evgeny Morozov <evgenydmorozov@gmail.com>
2022-04-25 15:01:13 +0500

    Создаёт файл миграции для постов

---
+++ updates/builder_table_create_yamobile_blog_post.php
@@ -0,0 +1,32 @@
+<?php namespace YaMobile\Blog\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableCreateYamobileBlogPost extends Migration
+{
+    public function up()
+    {
+        Schema::create('yamobile_blog_post', function($table)
+        {
+            $table->engine = 'InnoDB';
+            $table->increments('id')->unsigned();
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
+            $table->text('name');
+            $table->boolean('status');
+            $table->text('image')->nullable();
+            $table->text('description');
+            $table->text('content');
+            $table->text('meta_title')->nullable();
+            $table->text('meta_description')->nullable();
+            $table->text('meta_keywords')->nullable();
+            $table->text('slug');
+        });
+    }
+    
+    public function down()
+    {
+        Schema::dropIfExists('yamobile_blog_post');
+    }
+}
