c47086600711b00ecaf3978dabeaf99b0ca29d81
evgenydmorozov <evgenydmorozov@gmail.com>
2022-10-11 15:43:35 +0500

    Делает описание поста необязательным

---
+++ updates/builder_table_update_yamobile_blog_post.php
@@ -0,0 +1,23 @@
+<?php namespace YaMobile\Blog\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateYamobileBlogPost extends Migration
+{
+    public function up()
+    {
+        Schema::table('yamobile_blog_post', function($table)
+        {
+            $table->text('description')->nullable()->change();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('yamobile_blog_post', function($table)
+        {
+            $table->text('description')->nullable(false)->change();
+        });
+    }
+}
