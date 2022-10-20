541162eee5b26246b907c8d373608d791c7d15ec
evgenydmorozov <evgenydmorozov@gmail.com>
2022-10-20 17:06:57 +0500

    Добавляет возможность прикрепления тегов к записи

---
+++ updates/builder_table_create_yamobile_blog_posts_tags.php
@@ -0,0 +1,22 @@
+<?php namespace YaMobile\Blog\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableCreateYamobileBlogPostsTags extends Migration
+{
+    public function up()
+    {
+        Schema::create('yamobile_blog_posts_tags', function($table)
+        {
+            $table->engine = 'InnoDB';
+            $table->integer('post_id');
+            $table->smallInteger('tag_id');
+        });
+    }
+    
+    public function down()
+    {
+        Schema::dropIfExists('yamobile_blog_posts_tags');
+    }
+}
