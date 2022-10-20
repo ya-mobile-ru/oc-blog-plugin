86c62c566c87c2d60cc9e7542b8832cf675dd125
evgenydmorozov <evgenydmorozov@gmail.com>
2022-10-20 15:05:59 +0500

    Добавляет миграции для тегов

---
+++ updates/builder_table_create_yamobile_blog_tags.php
@@ -0,0 +1,24 @@
+<?php namespace YaMobile\Blog\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableCreateYamobileBlogTags extends Migration
+{
+    public function up()
+    {
+        Schema::create('yamobile_blog_tags', function($table)
+        {
+            $table->engine = 'InnoDB';
+            $table->increments('id')->unsigned();
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
+            $table->string('name');
+        });
+    }
+    
+    public function down()
+    {
+        Schema::dropIfExists('yamobile_blog_tags');
+    }
+}
