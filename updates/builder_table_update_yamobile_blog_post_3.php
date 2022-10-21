64a36e67894353f81ce999f1d6e96698d58db61d
evgenydmorozov <evgenydmorozov@gmail.com>
2022-10-21 15:55:53 +0500

    Добавляет h1 в записи

---
+++ updates/builder_table_update_yamobile_blog_post_3.php
@@ -0,0 +1,23 @@
+<?php namespace YaMobile\Blog\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateYamobileBlogPost3 extends Migration
+{
+    public function up()
+    {
+        Schema::table('yamobile_blog_post', function($table)
+        {
+            $table->text('h1')->nullable();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('yamobile_blog_post', function($table)
+        {
+            $table->dropColumn('h1');
+        });
+    }
+}
