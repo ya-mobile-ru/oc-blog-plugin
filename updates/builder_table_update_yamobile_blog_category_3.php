f0aea29d48b0caf12d430a4ac664891063684ceb
evgenydmorozov <evgenydmorozov@gmail.com>
2022-11-07 17:37:18 +0500

    Добавляет изображение к Категории

---
+++ updates/builder_table_update_yamobile_blog_category_3.php
@@ -0,0 +1,23 @@
+<?php namespace YaMobile\Blog\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateYamobileBlogCategory3 extends Migration
+{
+    public function up()
+    {
+        Schema::table('yamobile_blog_category', function($table)
+        {
+            $table->string('image')->nullable();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('yamobile_blog_category', function($table)
+        {
+            $table->dropColumn('image');
+        });
+    }
+}
