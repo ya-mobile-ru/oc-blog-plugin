79e6ed5e84380e3655a7bad9d9ae1e05b534f4b4
NiTika1509 <sicnax@gmail.com>
2023-06-25 22:53:26 +0300

    refactor(components): refactor post component

--- components/Post.php
+++ components/Post.php
@@ -2,10 +2,13 @@
 
 use Cms\Classes\ComponentBase;
 use Yamobile\Blog\Models\Post as BlogPost;
-use Yamobile\Blog\Models\Category;
 
 class Post extends ComponentBase
 {
+
+    public $post;
+    public $breadcrumbs;
+
     public function componentDetails()
     {
         return [
@@ -26,7 +29,20 @@ class Post extends ComponentBase
         ];
     }
 
-    public function post()
+    public function onRun()
+    {
+
+        $this->post = $this->loadPost();
+
+        if(!$this->post || ($this->post->category['slug'] !== $this->property('blog_slug'))) {
+            $this->setStatusCode(404);
+            return $this->controller->run('404');
+        }
+
+        $this->breadcrumbs = $this->generateBreadcrumbs();
+    }
+
+    private function loadPost()
     {
         $slug = $this->property('slug');
         
